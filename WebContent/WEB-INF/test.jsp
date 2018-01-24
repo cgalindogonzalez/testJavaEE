de290462fb137d8a4c65b215c10a2de800c845d4
Cecilia Galindo Gonzalez <cecilia@MacBook-Pro-de-Jaime.local>
2018-01-24 14:16:36 +0100

    add the bean transmissio to the servlet Test.java and show the bean properties by the jsp

--- WebContent/WEB-INF/test.jsp
+++ WebContent/WEB-INF/test.jsp
@@ -9,10 +9,21 @@
     <body>
         <p>Ceci est une page générée depuis une JSP.</p>
         <p>
-        		<%
-        			String attribut = (String )request.getAttribute("test");
-        			out.println(attribut);
-        		%>
+            <% 
+            String attribut = (String) request.getAttribute("test");
+            out.println( attribut );
+
+            String parametre = request.getParameter( "auteur" );
+            out.println( parametre );
+            %>
+        </p>
+        <p>
+            Récupération du bean :
+            <%  
+        com.sdzee.beans.Coyote notreBean = (com.sdzee.beans.Coyote) request.getAttribute("coyote");
+        out.println( notreBean.getPrenom() );
+            out.println( notreBean.getNom() );
+            %>
         </p>
     </body>
 </html>
--- src/com/sdzee/servlets/Test.java
+++ src/com/sdzee/servlets/Test.java
@@ -8,13 +8,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sdzee.beans.Coyote;
+
 public class Test extends HttpServlet {
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		/* Création et initialisation du message. */
 		String paramAuteur = request.getParameter("auteur");
 		String message = "Transmission de variables : OK ! " + paramAuteur;
-		request.setAttribute("test", message);
+		
+		/* Création du bean */
+	    Coyote premierBean = new Coyote();
+	    /* Initialisation de ses propriétés */
+	    premierBean.setNom( "Coyote" );
+	    premierBean.setPrenom( "Wile E." );
+	    
+	    /* Stockage du message et du bean dans l'objet request */
+	    request.setAttribute( "test", message );
+	    request.setAttribute( "coyote", premierBean );
+	    
+	    /* Transmission de la paire d'objets request/response à notre JSP */
 		this.getServletContext().getRequestDispatcher("/WEB-INF/test.jsp").forward(request, response);
 	}
 
