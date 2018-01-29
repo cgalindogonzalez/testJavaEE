567a785b4d024e5187e5609f30533bd713d1f704
cgalindogonzalez <cecilia.galindo.g@gmail.com>
2018-01-29 14:26:44 +0100

    test for and if using java code in the jsp file (against good practices)

--- src/com/sdzee/servlets/Test.java
+++ src/com/sdzee/servlets/Test.java
@@ -1,11 +1,15 @@
 package com.sdzee.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.joda.time.DateTime;
 
 import com.sdzee.beans.Coyote;
 
@@ -23,9 +27,25 @@ public class Test extends HttpServlet {
 	    premierBean.setNom( "Coyote" );
 	    premierBean.setPrenom( "Wile E." );
 	    
-	    /* Stockage du message et du bean dans l'objet request */
-	    request.setAttribute( "test", message );
-	    request.setAttribute( "coyote", premierBean );
+	    /* Création de la liste et insertion de quatre éléments */
+	    List<Integer> premiereList = new ArrayList<Integer>();
+	    premiereList.add(27);
+	    premiereList.add(12);
+	    premiereList.add(138);
+	    premiereList.add(6);
+	    
+	    /** On utilise ici la libraire Joda pour manipuler les dates, pour deux raisons :
+	     *    - c'est tellement plus simple et limpide que de travailler avec les objets Date ou Calendar !
+	     *    - c'est (probablement) un futur standard de l'API Java.
+	     */
+	    DateTime dt = new DateTime();
+	    Integer jourDuMois = dt.getDayOfMonth();
+	    
+	    /* Stockage du message, du bean, de la liste et du jour du mois dans l'objet request */
+	    request.setAttribute("test", message);
+	    request.setAttribute("coyote", premierBean);
+	    request.setAttribute("liste", premiereList);
+	    request.setAttribute("jour", jourDuMois);
 	    
 	    /* Transmission de la paire d'objets request/response à notre JSP */
 		this.getServletContext().getRequestDispatcher("/WEB-INF/test.jsp").forward(request, response);
